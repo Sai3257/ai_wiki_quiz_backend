010650c1c1bd41293162180e8d52816fa54d7bdf
sainath <sainath06123@gmail.com>
2025-11-09 18:30:12 +0530

    uvicorn fix

--- main.py
+++ main.py
@@ -183,5 +183,4 @@ def get_quiz_by_id(quiz_id: int, db: Session = Depends(get_db)):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
