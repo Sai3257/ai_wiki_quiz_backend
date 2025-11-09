97700db053eb77a74835b85822f6a32a3570f0c5
sainath <sainath06123@gmail.com>
2025-11-09 18:16:09 +0530

    changed cors

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "https://aiwikiquizfrontend-production.up.railway.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
